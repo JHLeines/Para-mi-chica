c56402eecd76eca9c25d5bda93525155af75c6ee
JHLeines <omgelterry@gmail.com>
2025-02-02 00:44:04 -0600

    Delete script2.js

--- script2.js
+++
@@ -1,9 +0,0 @@
-/* script.js */
-document.addEventListener("DOMContentLoaded", function() {
-    const card = document.querySelector(".card");
-    card.style.opacity = 0;
-    setTimeout(() => {
-        card.style.transition = "opacity 2s";
-        card.style.opacity = 1;
-    }, 500);
-});
